7086258544133f0f83c12aaab47b1f3a610c3b27
Hans Hoffman <github.backrest158@passmail.net>
2025-01-06 16:41:08 -0700

    add better error reporting

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,36 @@ const std = @import("std");
 const lexer = @import("lexer.zig");
 const parser = @import("parser.zig");
 
+fn showErrorContext(source: []const u8, line: usize, column: usize) void {
+    var current_line: usize = 1;
+    var last_newline: usize = 0;
+    var i: usize = 0;
+
+    // Find the start of the error line
+    while (i < source.len) : (i += 1) {
+        if (current_line == line) break;
+        if (source[i] == '\n') {
+            current_line += 1;
+            last_newline = i + 1;
+        }
+    }
+
+    // Find the end of the error line
+    var line_end = last_newline;
+    while (line_end < source.len and source[line_end] != '\n') : (line_end += 1) {}
+
+    // Print the line with error
+    const line_content = source[last_newline..line_end];
+    std.debug.print("\n{d} | {s}\n", .{ line, line_content });
+
+    // Print the error pointer
+    std.debug.print("  | ", .{});
+    for (0..column - 1) |_| {
+        std.debug.print(" ", .{});
+    }
+    std.debug.print("^\n", .{});
+}
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -28,7 +58,24 @@ pub fn main() !void {
     defer tokens.deinit();
 
     while (true) {
-        const token = try tokenizer.nextToken();
+        const token = tokenizer.nextToken() catch |err| {
+            switch (err) {
+                error.InvalidCharacter => {
+                    std.debug.print("\nError: Invalid character at line {d}, column {d}\n", .{ tokenizer.line, tokenizer.column });
+                    showErrorContext(source, tokenizer.line, tokenizer.column);
+
+                    return err;
+                },
+                error.UnterminatedString => {
+                    std.debug.print("\nError: Unterminated string starting at line {d}, column {d}\n", .{ tokenizer.line, tokenizer.column });
+                    showErrorContext(source, tokenizer.line, tokenizer.column);
+
+                    return err;
+                },
+                else => return err,
+            }
+        };
+
         try tokens.append(token);
 
         if (token.kind == .Eof) break;
